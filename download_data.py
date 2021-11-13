625fec2d817b6537d757e3a8932208b715df3aed
martiponsmir <80447676+martiponsmir@users.noreply.github.com>
2021-11-13 16:28:32 +0100

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import numpy as np
 
-lloguer2014 = pd.read_csv("2014_lloguer_preu_trim.csv")
-lloguer2015 = pd.read_csv("2015_lloguer_preu_trim.csv")
-lloguer2016 = pd.read_csv("2016_lloguer_preu_trim.csv")
-lloguer2017 = pd.read_csv("2017_lloguer_preu_trim.csv")
-lloguer2018 = pd.read_csv("2018_lloguer_preu_trim.csv")
-lloguer2019 = pd.read_csv("2019_lloguer_preu_trim.csv")
-lloguer2020 = pd.read_csv("2020_lloguer_preu_trim.csv")
-lloguer2021 = pd.read_csv("2021_lloguer_preu_trim.csv")
+lloguers = []
+for i in range(2014, 2022):
+    file = str(i)+"_lloguer_preu_trim.csv"
+    ll = pd.read_csv(file)
+    ll = ll.loc[(ll["Lloguer_mitja"]=="Lloguer mitjà mensual (Euros/mes)")]
+    lloguers.append(ll)
+
+lloguer = pd.concat(lloguers)
