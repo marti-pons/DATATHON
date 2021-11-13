67beb5f7f810b44e470e731f59923c269290f4e3
martiponsmir <80447676+martiponsmir@users.noreply.github.com>
2021-11-13 16:33:44 +0100

    download lloguer i renda

--- download_data.py
+++ download_data.py
@@ -7,4 +7,12 @@ for i in range(2014, 2022):
     ll = ll.loc[(ll["Lloguer_mitja"]=="Lloguer mitjà mensual (Euros/mes)")]
     lloguers.append(ll)
 
-lloguer = pd.concat(lloguers)
+LLOGUER = pd.concat(lloguers)
+
+rendes = []
+for i in range(2015, 2019):
+    file = str(i)+"_renda_disponible_llars.csv"
+    r = pd.read_csv(file)
+    rendes.append(r)
+
+RENDA = pd.concat(rendes)
