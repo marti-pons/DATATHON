1e7f12f7070d7d7a3f14bae4109428e264cd1701
martiponsmir <80447676+martiponsmir@users.noreply.github.com>
2021-11-13 15:46:24 +0100

    read_csv lloguer

--- download_data.py
+++ download_data.py
@@ -1,27 +1,11 @@
 import pandas as pd
 import numpy as np
 
-# and remove other columns
-
-df = pd.read_excel('../dataset_renta.xlsx', header = 8, 
-                   usecols = [0,4,5,6,7])
-
-# We replace the names of the variables from the dataset
-
-df.columns = ['Barri', 'T1', 'T2', 'T3', 'T4']
-
-# dropna() removes all the rows with nan values
-df = df.dropna()
-
-# You can remove rows, in this case the first one
-df = df.drop(index = 0)
-
-
-# Some of the datasets can have blank spaces in the string values, with a simple map a lambda function and the
-# function lstrip  (left-strip) you can get rido of certain characters
-df.Barri.map(lambda x : x.lstrip('     '))
-
-# Again, it can be that some string have other no desirable characters, you can first replace them to blank spaces
-# and then remove all of them from certain columns
-df[['Districte', 'Barri']] = [i.replace('\xa0', ' ').split(' ', 1) for i in df.Barri]
-[i.lstrip(' ').rstrip(' ') for i in df.Barri]
+lloguer2014 = pd.read_csv("2014_lloguer_preu_trim.csv")
+lloguer2015 = pd.read_csv("2015_lloguer_preu_trim.csv")
+lloguer2016 = pd.read_csv("2016_lloguer_preu_trim.csv")
+lloguer2017 = pd.read_csv("2017_lloguer_preu_trim.csv")
+lloguer2018 = pd.read_csv("2018_lloguer_preu_trim.csv")
+lloguer2019 = pd.read_csv("2019_lloguer_preu_trim.csv")
+lloguer2020 = pd.read_csv("2020_lloguer_preu_trim.csv")
+lloguer2021 = pd.read_csv("2021_lloguer_preu_trim.csv")
